e9bc6b179177786874e12498c10609ee7bffc7c9
shobhit Sinha <shobhit.s1813@gmail.com>
2020-01-08 10:31:01 +0530

    bug in quantity in cart resolved partially

--- src/app/food-home-page/featured/popularbrand/popularbrand.component.ts
+++ src/app/food-home-page/featured/popularbrand/popularbrand.component.ts
@@ -17,6 +17,7 @@ export class PopularbrandComponent implements OnInit {
   cartItem: any[];
   noOfItems: number;
   isRestaurant: boolean = true;
+  alreadyPresent: boolean;
   constructor(private service: FoodServiceService, private _customizeDialog: MatDialog, private _bottomSheet: MatBottomSheet) { }
 
   ngOnInit() {
@@ -25,6 +26,7 @@ export class PopularbrandComponent implements OnInit {
         this.popularBrandList = data.list;
         this.noOfItems = this.popularBrandList.length;
         this.isCartNeeded = true;
+        this.alreadyPresent = false;
       }
     })
 
@@ -49,8 +51,9 @@ export class PopularbrandComponent implements OnInit {
     this.cartItem.filter((data, index) => {
       if (data.name === itemSelected.name) {
         console.log("the item selected is ", itemSelected.quantity);
-        itemSelected.quantity++;
-        return this.cartItem.push(this.cartItem[index]);
+        itemSelected.quantity = itemSelected.quantity + 1;
+        console.log("the item selected after increase", itemSelected.quantity)
+        // return this.cartItem.push(this.cartItem[index]);
       }
     })
     return this.cartItem;
@@ -58,31 +61,21 @@ export class PopularbrandComponent implements OnInit {
 
   openCustomizeOptionDialog(itemSelected) {
     if (itemSelected !== undefined && itemSelected.customize !== undefined) {
+      itemSelected.quantity = parseInt(itemSelected.quantity)+1;
       const customizeRef = this._customizeDialog.open(CustomDialogboxComponent, {
         width: '500px',
         data: { item: itemSelected, customize: itemSelected.customize }
       });
       customizeRef.afterClosed().subscribe(customizedValue => {
-        this.cartItem.filter((data) => {
-          if (customizedValue.name === data.name && customizedValue.selectedOptions === data.customized_options) {
-            console.log("inisde if Condition");
-            data.quantity++;
-          }
-          else {
-            console.log("in else condition");
-            const customized_price = parseInt(itemSelected.price) + parseInt(customizedValue.total_price);
-            this.cartItem.push(JSON.parse('{"name":"' + customizedValue.name + '","customized_price":"' + customized_price + '","quantity":"' + customizedValue.quantity + '","customized_options":"' + customizedValue.selectedOptions + '"}'));
-          }
-        })
-      }
-      )
+          const customized_price = parseInt(itemSelected.price) + parseInt(customizedValue.total_price);
+          this.cartItem.push(JSON.parse('{"name":"' + customizedValue.name + '","customized_price":"' + customized_price + '","quantity":"' + customizedValue.quantity + '","customized_options":"' + customizedValue.selectedOptions + '"}'));
+        
+      });
     }
     else {
-      console.log("in outer else")
       this.cartItem.push(itemSelected);
     }
     return this.cartItem;
   }
-
 }
 // https://stackoverflow.com/questions/43223582/why-angular-2-ngonchanges-not-responding-to-input-array-push
